95390726b500faf212bfbf2c89d58c477699313c
Peter SZTANOJEV <sztanpet@gmail.com>
2013-07-10 20:26:50 +0200

    angel process to supervise the chat server and restart it as needed

---
+++ angel/debug.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	//"github.com/emicklei/hopwatch"
+	"log"
+	"runtime"
+	"time"
+)
+
+// source https://groups.google.com/forum/?fromgroups#!topic/golang-nuts/C24fRw8HDmI
+// from David Wright
+type ErrorTrace struct {
+	err   error
+	trace string
+}
+
+func NewErrorTrace(v ...interface{}) error {
+	msg := fmt.Sprint(v...)
+	pc, file, line, ok := runtime.Caller(2)
+	if ok {
+		fun := runtime.FuncForPC(pc)
+		loc := fmt.Sprint(fun.Name(), "\n\t", file, ":", line)
+		return ErrorTrace{err: errors.New(msg), trace: loc}
+	}
+	return errors.New("error generating error")
+}
+
+func (et ErrorTrace) Error() string {
+	return et.err.Error() + "\n  " + et.trace
+}
+
+func B(v ...interface{}) {
+	ts := time.Now().Format("2006-02-01 15:04:05: ")
+	println(ts, NewErrorTrace(v...).Error())
+	panic("-----")
+}
+
+func D(v ...interface{}) {
+	if debuggingenabled {
+		log.Println(v...)
+	}
+}
+
+func DP(v ...interface{}) {
+	if debuggingenabled {
+		log.Print(v...)
+	}
+}
+
+func P(v ...interface{}) {
+	log.Println(v...)
+}
---
+++ angel/main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"code.google.com/p/go.net/websocket"
+	conf "github.com/msbranco/goconfig"
+	"log"
+	"os"
+	"os/exec"
+	"os/signal"
+	"path"
+	"strings"
+	"syscall"
+	"time"
+)
+
+var debuggingenabled bool
+
+func main() {
+	c, err := conf.ReadConfigFile("angel.cfg")
+	if err != nil {
+		nc := conf.NewConfigFile()
+		nc.AddOption("default", "debug", "false")
+		nc.AddOption("default", "binarypath", "./")
+		nc.AddOption("default", "serverurl", "ws://localhost:9998/ws")
+		nc.AddOption("default", "origin", "http://localhost")
+
+		if err := nc.WriteConfigFile("angel.cfg", 0644, "Chat Angel, watching over the chat and restarting it as needed"); err != nil {
+			log.Fatal("Unable to create angel.cfg: ", err)
+		}
+		if c, err = conf.ReadConfigFile("angel.cfg"); err != nil {
+			log.Fatal("Unable to read angel.cfg: ", err)
+		}
+	}
+
+	debuggingenabled, _ = c.GetBool("default", "debug")
+	binpath, _ := c.GetString("default", "binarypath")
+	serverurl, _ := c.GetString("default", "serverurl")
+	origin, _ := c.GetString("default", "origin")
+
+	base := path.Base(binpath)
+	basedir := strings.TrimSuffix(binpath, "/"+base)
+	os.Chdir(basedir) // so that the chat can still read the settings.cfg
+
+	shouldrestart := make(chan bool)
+	processexited := make(chan bool)
+	sct := make(chan os.Signal, 1)
+	signal.Notify(sct, syscall.SIGTERM)
+
+	go (func() {
+		for _ = range sct {
+			log.Println("CAUGHT SIGTERM, restarting the chat")
+			shouldrestart <- true
+		}
+	})()
+
+again:
+	cmd := exec.Command(binpath)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	laststarttime := time.Now()
+	if err := cmd.Start(); err != nil {
+		D("Error starting", binpath, err)
+		return
+	}
+
+	go (func() {
+		if err := cmd.Wait(); err != nil {
+			D("Error while waiting for process to exit ", err)
+		}
+		D("Chat process exited, restarting")
+		processexited <- true
+	})()
+
+	time.Sleep(1 * time.Second)
+	go checkResponse(serverurl, origin, shouldrestart)
+	t := time.NewTicker(500 * time.Millisecond)
+
+	for {
+		select {
+		case <-t.C:
+			go checkNames(serverurl, origin, shouldrestart)
+		case <-shouldrestart:
+			cmd.Process.Signal(syscall.SIGINT)
+			// TODO move pprof files out of the dir
+		case <-processexited:
+			if time.Now().Sub(laststarttime) < 5*time.Second {
+				D("Tried restarting the chat process too fast, sleeping for 5 seconds")
+				time.Sleep(5 * time.Second)
+			}
+			goto again
+		}
+	}
+}
+
+func checkResponse(serverurl, origin string, shouldrestart chan bool) {
+	ws, err := websocket.Dial(serverurl, "", origin)
+	if err != nil {
+		D("Unable to connect to ", serverurl)
+		shouldrestart <- true
+		return
+	}
+
+	defer ws.Close()
+
+	buff := make([]byte, 512)
+	for {
+		// the pinginterval is 10 seconds on the server side, we HAVE TO receive something
+		ws.SetReadDeadline(time.Now().Add(25 * time.Second))
+		len, err := ws.Read(buff)
+		if err != nil {
+			D("Unable to read from the websocket", err)
+			shouldrestart <- true
+			return
+		}
+
+		if debuggingenabled {
+			D("checkResponse received:", string(buff[:len]))
+		}
+
+		if string(buff[:4]) == "PING" {
+			ws.SetWriteDeadline(time.Now().Add(time.Second))
+			buff[1] = 'O'
+			_, err = ws.Write(buff[:len])
+			if err != nil {
+				D("Unable to write to the websocket", err)
+				shouldrestart <- true
+				return
+			}
+		}
+	}
+}
+
+func checkNames(serverurl, origin string, shouldrestart chan bool) {
+	ws, err := websocket.Dial(serverurl, "", origin)
+	defer ws.Close()
+	if err != nil {
+		D("Unable to connect to ", serverurl)
+		shouldrestart <- true
+		return
+	}
+
+	buff := make([]byte, 512)
+	ws.SetReadDeadline(time.Now().Add(time.Second))
+	_, err = ws.Read(buff)
+	if err != nil {
+		D("Unable to read from the websocket ", err)
+		shouldrestart <- true
+		return
+	}
+
+	if string(buff[:5]) != "NAMES" && string(buff[:4]) != "JOIN" {
+		D("Unexpected data received: ", string(buff))
+		shouldrestart <- true
+		return
+	}
+
+	if debuggingenabled {
+		D("checkNames received NAMES or JOIN, exiting, data was:", string(buff))
+	}
+}
