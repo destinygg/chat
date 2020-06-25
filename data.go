fd4478dca5a883d3a9760de4b5917dc735617b19
Dowland Aiello <dowlandaiello@gmail.com>
2020-06-25 14:11:34 -0400

    data: Make pack and unpack functions more idiomatic + add documentation.
    Signed-off-by: Dowland Aiello <dowlandaiello@gmail.com>

--- data.go
+++ data.go
@@ -6,24 +6,19 @@ import (
 	"strings"
 )
 
-func Unpack(data string) (string, []byte, error) {
-	result := strings.SplitN(data, " ", 2)
-	if len(result) != 2 {
+// Unpack unpacks an event name, and a byte-slice data from a packed data string.
+func Unpack(packed string) (string, []byte, error) {
+	// The event name, followed by the data as a string
+	parts := strings.SplitN(packed, " ", 2)
+
+	if len(parts) != 2 {
 		return "", nil, errors.New("Unable to extract event name from data.")
 	}
-	return result[0], []byte(result[1]), nil
+
+	return parts[0], []byte(parts[1]), nil
 }
 
-func Unmarshal(data []byte, out interface{}) error {
-	return json.Unmarshal(data, out)
-}
-
-func Marshal(out interface{}) ([]byte, error) {
-	return json.Marshal(out)
-}
-
-func Pack(name string, data []byte) ([]byte, error) {
-	result := []byte(name + " ")
-	result = append(result, data...)
-	return result, nil
+// Pack packs an event name and byte data into a string formatted as such: "event_name data_as_string".
+func Pack(name string, data []byte) (result []byte) {
+	result = append([]byte(name+" "), data...)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/destinygg/chat
 
 go 1.14
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gorilla/websocket v1.4.2
+	github.com/msbranco/goconfig v0.0.0-20160629072055-3189001257ce
+	github.com/pquerna/ffjson v0.0.0-20190930134022-aa0246cd15f7
+	github.com/tideland/golib v4.24.2+incompatible
+)
